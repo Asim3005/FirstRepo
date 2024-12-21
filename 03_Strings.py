d6f8f8cf326c54b84c56942aba50df15be185375
Muhammad Asim Mushtaq <asimmushtaq3005@gmail.com>
2024-12-21 08:12:34 +0500

    my first commit

---
+++ 03_Strings.py
@@ -0,0 +1,9 @@
+# print("Hello world")
+# print("We are learning python")
+
+# String are any text which is used in excel in formate of ' '  " " ''' ''' are called string.
+# print('Test for single quotes')
+# print("test for double quotes")
+# print('''test for triple quotes''')
+
+print("what's up ")
